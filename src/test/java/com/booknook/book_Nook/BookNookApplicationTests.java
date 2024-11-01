09d7d5da251b5b0b9f8fe77b224c1592a47ce4ac
rpaternina <robertpaternina65@gmail.com>
2024-10-31 19:06:13 -0500

    Subiendo a remoto

---
+++ src/test/java/com/booknook/book_Nook/BookNookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booknook.book_Nook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
